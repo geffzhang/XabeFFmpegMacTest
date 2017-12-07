af93fe982f76ff163babeba4a03c96d0f684a053
Nabil Asadi <nabilassadi@HEILAP66.local>
2017-12-07 09:43:50 -0700

    mac run

--- XabeFFmpegMacTest/Program.cs
+++ XabeFFmpegMacTest/Program.cs
@@ -31,15 +31,16 @@ namespace XabeFFmpegMacTest
             exist = File.Exists(songMp4);
             Console.WriteLine(exist + "-" + songMp4);
 
-            var mp3OutPut = Path.Combine(root, "songAudio.mp3");
+            var mp3OutPut = Path.Combine(root, DateTime.Now.ToString("yyyyMMddHHmmssfff") + "songAudio.mp3");
 
             bool result = await ConversionHelper.ExtractAudio(songMp4, mp3OutPut).Start();
 
             Console.WriteLine($"\nMp3 Extracted\n{ new MediaInfo(mp3OutPut)}\n");
             Console.ReadLine();
 
-
-/* Windows 10 output
+/*
+----------------------------------------------------------------
+Windows 10 output
  
 True - C:\Users\code\XabeFFmpegMacTest\XabeFFmpegMacTest\ffmpeg\win64
 True - C:\Users\code\XabeFFmpegMacTest\XabeFFmpegMacTest\song.mp4
@@ -57,12 +58,50 @@ Aspect Ratio :
 Framerate: fps
 Resolution : 0 x 0
 Size: 4706566 b
+
+---------------------------------------------------------------
+Mac Output:
+
+True-/Users/code/XabeFFmpegMacTest/XabeFFmpegMacTest/ffmpeg/mac64
+True-/Users/code/XabeFFmpegMacTest/XabeFFmpegMacTest/song.mp4
+
+Unhandled Exception: System.ComponentModel.Win32Exception: Permission denied
+   at Interop.Sys.ForkAndExecProcess(String filename, String[] argv, String[] envp, String cwd, Boolean redirectStdin, Boolean redirectStdout, Boolean redirectStderr, Int32& lpChildPid, Int32& stdinFd, Int32& stdoutFd, Int32& stderrFd)
+   at System.Diagnostics.Process.StartCore(ProcessStartInfo startInfo)
+   at System.Diagnostics.Process.Start()
+   at Xabe.FFmpeg.FFbase.RunProcess(String args, String processPath, Boolean rStandardInput, Boolean rStandardOutput, Boolean rStandardError)
+   at Xabe.FFmpeg.FFmpeg.<>c__DisplayClass9_0.<RunProcess>b__0()
+   at System.Threading.Tasks.Task`1.InnerInvoke()
+   at System.Threading.ExecutionContext.Run(ExecutionContext executionContext, ContextCallback callback, Object state)
+   at System.Threading.Tasks.Task.ExecuteWithThreadLocal(Task& currentTaskSlot)
+--- End of stack trace from previous location where exception was thrown ---
+   at System.Runtime.ExceptionServices.ExceptionDispatchInfo.Throw()
+   at System.Runtime.CompilerServices.TaskAwaiter.HandleNonSuccessAndDebuggerNotification(Task task)
+   at Xabe.FFmpeg.FFmpeg.<RunProcess>d__9.MoveNext()
+--- End of stack trace from previous location where exception was thrown ---
+   at System.Runtime.ExceptionServices.ExceptionDispatchInfo.Throw()
+   at System.Runtime.CompilerServices.TaskAwaiter.HandleNonSuccessAndDebuggerNotification(Task task)
+   at Xabe.FFmpeg.Conversion.<Start>d__37.MoveNext()
+--- End of stack trace from previous location where exception was thrown ---
+   at System.Runtime.ExceptionServices.ExceptionDispatchInfo.Throw()
+   at System.Runtime.CompilerServices.TaskAwaiter.HandleNonSuccessAndDebuggerNotification(Task task)
+   at Xabe.FFmpeg.Conversion.<Start>d__36.MoveNext()
+--- End of stack trace from previous location where exception was thrown ---
+   at System.Runtime.ExceptionServices.ExceptionDispatchInfo.Throw()
+   at System.Runtime.CompilerServices.TaskAwaiter.HandleNonSuccessAndDebuggerNotification(Task task)
+   at Xabe.FFmpeg.Conversion.<Start>d__34.MoveNext()
+--- End of stack trace from previous location where exception was thrown ---
+   at System.Runtime.ExceptionServices.ExceptionDispatchInfo.Throw()
+   at System.Runtime.CompilerServices.TaskAwaiter.HandleNonSuccessAndDebuggerNotification(Task task)
+   at System.Runtime.CompilerServices.TaskAwaiter`1.GetResult()
+   at XabeFFmpegMacTest.Program.<Main>d__0.MoveNext() in /Users/code/XabeFFmpegMacTest/XabeFFmpegMacTest/Program.cs:line 36
+--- End of stack trace from previous location where exception was thrown ---
+   at System.Runtime.ExceptionServices.ExceptionDispatchInfo.Throw()
+   at System.Runtime.CompilerServices.TaskAwaiter.HandleNonSuccessAndDebuggerNotification(Task task)
+   at XabeFFmpegMacTest.Program.<Main>(String[] args)
+The program '[6780] XabeFFmpegMacTest.dll' has exited with code 0 (0x0).
+ 
 */
-
-
-
-
-
 
         }
 
